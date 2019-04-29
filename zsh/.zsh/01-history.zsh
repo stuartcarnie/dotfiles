520613e686f236982bc5189c9f2a4103dc5c384b
Stuart Carnie <stuart.carnie@gmail.com>
2019-04-29 11:39:45 -0700

    feat: increase Zsh history size

--- zsh/.zsh/01-history.zsh
+++ zsh/.zsh/01-history.zsh
@@ -6,3 +6,11 @@
 #
 
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+
+
+#
+# Variables
+#
+
+HISTSIZE=100000                  # The maximum number of events to save in the internal history.
+SAVEHIST=100000                  # The maximum number of events to save in the history file.
