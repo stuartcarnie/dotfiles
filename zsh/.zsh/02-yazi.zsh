af242afd55d7a482610002da2cf7827443cd5a9c
Stuart Carnie <stuart.carnie@gmail.com>
2024-12-28 06:18:56 -0700

    Add y() function for yazi

---
+++ zsh/.zsh/02-yazi.zsh
@@ -0,0 +1,10 @@
+if is-callable 'yazi'; then
+  function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+  }
+fi
